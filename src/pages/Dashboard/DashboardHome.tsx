b9f45dc3e5aaddb13d6823f33b387149a86b9185
shaikot07 <saifulislamshaikat007@gmail.com>
2026-02-03 15:19:15 +0600

    updated

--- src/pages/Dashboard/DashboardHome.tsx
+++ src/pages/Dashboard/DashboardHome.tsx
@@ -3,7 +3,7 @@ import me from "@/assets/owner.png";
 export default function DashboardHome() {
   return (
     <div>
-      Dashboard Home page bro
+      <h2 className="text-2xl text-green-700">Dashboard Home page bro</h2>
       <div className="text-amber-200 ">
         <img src={me} alt="" />
       </div>
